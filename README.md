8bb7c8ffa89108b672d42596555c99788b6c5a93
tony <driemworks@DESKTOP-RN9BJOQ.localdomain>
2023-03-27 20:39:44 -0500

    32 bit pubkey generation works

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# Magi
+# DKG/VSS 
 
 This is a library for threshold cryptography. Specifically, it is a verifiable secret sharing scheme intended to be used on top of a distributed network.
 
 ## Overview
 
 This is an implementation of the scheme detailed in [Blind DKG]().
+
+Ok so I have an elliptic curve group, and my coefficients are coming from a finite field of order r.
+Now, what I'm trying to do is derive a new public key. Mathematically speaking, this is done by taking the product of the group generator raised to each secret.
+However, this is where I'm facing a huge challenge.
+
+So, there are two main types in use, Projective<Config> for group elements, and Fp<MontBackend<FrConfig, 4>, 4> for field elements.
+The group generator is given a a Projective<Config>, however, the coefficients (i.e. secrets) that I'm generating are in the field...
+So, if I want to calculate the product of the group generator exponentiated to a field element... argh this is confusing. 
 
 ### DKG
 This is a proof of concept distributed key generation protocol. The protocol is based on the Feldman VSS scheme. The intention is to augment the design, similar to the ethDKG protocol, to use ZK SNARKs within the disputes process.
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::{
     time::SystemTime,
     ops::Mul,
 };
-use ark_ec::{Group};
-use ark_ff::{MontBackend, Fp};
+use ark_ec::{Group, bls12::Bls12};
+use ark_ff::{MontBackend, Fp, PrimeField};
 use ark_test_curves::{
-    secp256k1::{
-        Config, G1Projective as G, Fr as ScalarField,
+    ed_on_bls12_381::{
+        EdwardsConfig as Config, Projective as G, Fr as ScalarField, FrConfig as FieldConfig,
     },
-    Field, short_weierstrass::Projective
+    Field,
 };
 use ark_poly::{
     polynomial::univariate::DensePolynomial,
@@ -22,128 +22,142 @@ use rand_chacha::{
 	rand_core::SeedableRng,
 };
 
+/// In the first go through, we make some assumptions that make things really simple
+/// First, we assume that each 'actor' or participant can only have one secret share at a time (i.e. can only participate in one society)
+/// And we also assume that all society communication/network broadcasts are atomic
 fn main() {
     let t = 2;
     let n = 3;
-    let g = G::generator();
-    let rng = ChaCha20Rng::seed_from_u64(31u64);
+    let rng = ChaCha20Rng::seed_from_u64(29u64);
     // create n actors
-    let actors = (1..n).map(|i| Actor{ slot: i } ).collect();
+    let actors = (1..n).map(|i| Actor{ slot: i, share: None } ).collect();
     // create a new society
-    let society = Society::new(actors, n, t, g);
+    let society = Society::new(actors, n, t);
     // generate keys
     let shares = society.dkg(rng);
+    // ASSUME OR SIMULATE: sharing phase. This is all on one machine.
+    // SKIP: DISPUTES (verify, unverify, zk snarks); For now we assume all values are valid.
+    // the secret shares are the first element of each of the abov 
+    let secret_shares = shares.iter().map(|x| x[0]).collect();
     // derive a public key
-    society.derive_pubkey(g, shares);
+    let mpk = society.derive(secret_shares);
+    let big_mpk: num_bigint::BigUint = mpk.to_owned().into();
+    let pubkey_bytes = big_mpk.to_bytes_le();
+    println!("Calculated a public key: {:?}", pubkey_bytes);
+    println!("Calculated a public key w/ len: {:?}", pubkey_bytes.len());
     // encrypt something
     //share it
     // decrypt it
 
 }
 
-pub struct KeyShare;
-pub struct Commitment;
-
-#[derive(Clone)]
-pub struct PublicVerificationKey {
-    pub coefficients: Vec<Projective<Config>>,
-}
-
-impl PublicVerificationKey {
-    /// evaluate the pkv at some point x
-    /// $f(x) = C_0 * C_1^{x^1} * ... * C_t^{x^t}$
-    pub fn evaluate(&self, x: ScalarField) -> Projective<Config> {
-        let mut result = self.coefficients[0];
-        for i in 1..self.coefficients.len() {
-            let big_i: num_bigint::BigUint = ScalarField::from(i as i32).into();
-            result = self.coefficients[i].mul(x.pow(big_i.to_u64_digits()));
-        }
-        result
-    }
-}
-
 pub struct Society {
     // will be made blind eventually
     pub participants: Vec<Actor>,
     pub shares: u8,
-    pub threshold: u8, 
-    pub generator: Projective<Config>,
+    pub threshold: u8,
 }
 
+/// A society is a collection of participants that can generate shares amongst themselves,
+/// derive a pubkey, and reencrypt a secret
 impl Society {
-    fn new(participants: Vec<Actor>, shares: u8, threshold: u8, generator: Projective<Config>) -> Self {
+    fn new(participants: Vec<Actor>, shares: u8, threshold: u8) -> Self {
         Self {
-            participants, shares, threshold, generator,
+            participants, shares, threshold,
         }
     }
 
-    /// ultimately issue (commitment, share)
-    fn dkg(&self, rng: ChaCha20Rng) -> 
-            Vec<Vec<(Fp<MontBackend<ark_test_curves::secp256k1::FrConfig, 4>, 4>, ark_ec::short_weierstrass::Projective<ark_test_curves::secp256k1::Config>)>> 
-        {
+    /// the main distributed key generation algorithm
+    /// each member of the society generates shares
+    /// 
+    /// * rng: the random number generator
+    /// 
+    fn dkg(&self, rng: ChaCha20Rng) -> Vec<Vec<ScalarField>> {
         let mut shares = Vec::new();
         for p in self.participants.iter() {
             // each participant generates shares
-            let participant_shares = p.generate_shares(self.shares, self.threshold, rng.clone(), self.generator);
+            let (_, participant_shares) = 
+                p.generate_shares(self.shares, self.threshold, rng.clone());
             shares.push(participant_shares);
-        }   
-        // ASSUME OR SIMULATE: sharing phase. This is all on one machine.
-        // SKIP: DISPUTES; For now we assume all values are valid.
+        }
         shares
     }
 
     /// Given a group generator 'g', the public key is the product of the group generator raised to all secret keys
-    /// that is, mpk = sum(g^{s}; for all secrets s)
+    /// that is, mpk = product(g^{s}) for all secrets s
     /// 
     /// `generator`: The generator to be used to calculate a public key
-    /// 
-    fn derive_pubkey(
-        &self, 
-        generator: Projective<Config>, 
-        shares: Vec<Vec<(Fp<MontBackend<ark_test_curves::secp256k1::FrConfig, 4>, 4>, ark_ec::short_weierstrass::Projective<ark_test_curves::secp256k1::Config>)>>)  {
-        // TODO
+    ///
+    fn derive(&self, shares: Vec<Fp<MontBackend<FieldConfig, 4>, 4>>) -> ScalarField  {
+        // let modulus = <ScalarField as PrimeField>::MODULUS;
+        // let g = G::generator();
+        let g = ScalarField::from(2i32);
+        // should be 'actor' based instead of iterating over the shares, we should iterate over actors
+        // then each actor can derive a pubkey independently. 
+        let public_key_shares = shares.iter().map(|share| {
+            let big_share: num_bigint::BigUint = share.to_owned().into();
+            g.pow(big_share.to_u64_digits())
+        }).collect::<Vec<_>>();
+        let mut mpk = public_key_shares[0];
+        for i in 1..shares.len() {
+            mpk = mpk * shares[i];
+        }
+        ScalarField::from(mpk)
+    }
+
+    fn reencrypt() {
+        todo!("Not yet implemented");
     }
 
 }
+
 
 /// a participant in the protocol
 pub struct Actor {
     // todo
     pub slot: u8,
+    pub share: Option<ScalarField>,
 }
 
 impl Actor {
-
-    fn new(slot: u8) -> Self {
-        Actor{ slot }
-    }
-    /// generates a random polynomial over the given field
-    /// then calculates shares and commitments and returns them
-    pub fn generate_shares<R: Rng + Sized>(&self, n: u8, t: u8, mut r: R, g: Projective<Config>) -> Vec<(Fp<MontBackend<ark_test_curves::secp256k1::FrConfig, 4>, 4>, Projective<Config>)>{
+    /// generates a random polynomial over the given field, then calculates shares
+    pub fn generate_shares<R: Rng + Sized>(&self, n: u8, t: u8, mut r: R) 
+        -> (DensePolynomial<ScalarField>, Vec<Fp<MontBackend<FieldConfig, 4>, 4>>) {
         let rand_poly = DensePolynomial::<ScalarField>::rand(t as usize, &mut r);
-        let commitment_poly = generate_poly_commitment(g, rand_poly.clone());
-         (0..n).map(|k| {
-            let secret_share = rand_poly.clone().evaluate(&ScalarField::from(k));
-            let commitment = commitment_poly.clone().evaluate(ScalarField::from(k));
-            (secret_share, commitment)
-        }).collect::<Vec<_>>()
+        let shares = (0..n).map(|k| {
+            rand_poly.clone().evaluate(&ScalarField::from(k))
+        }).collect::<Vec<_>>();
+        (rand_poly, shares)
     }
+
+    pub fn derive_pubkey(g: ScalarField) {
+
+    }
+
 }
-// each coefficient's commitment is $g^{coefficient}$
-// so then, I can just do the product of those, generate coefficients 
-// for the new polynomial
-//
-// i.e. I'm taking $f(x) = c_0 + c_1 * x + ... + c_t x^t$
-// then commiting to them and defining new coefficients for the public verification key
-// $[g^{c_0}, g^{c_1}, ..., g^{c_n}]$
-//
-fn generate_poly_commitment(g: Projective<Config>, poly: DensePolynomial<ScalarField>) -> PublicVerificationKey {
-    let commitments: Vec<Projective<Config>> = poly.coeffs().iter().map(|c: &ScalarField| {
-        // commitment = g^c
-        g.mul(c)
-    }).collect::<Vec<_>>();
-    PublicVerificationKey { coefficients: commitments }
-}
+// // each coefficient's commitment is $g^{coefficient}$
+// // so then, I can just do the product of those, generate coefficients 
+// // for the new polynomial
+// //
+// // i.e. I'm taking $f(x) = c_0 + c_1 * x + ... + c_t x^t$
+// // then commiting to them and defining new coefficients for the public verification key
+// // $[g^{c_0}, g^{c_1}, ..., g^{c_n}]$
+// //
+// fn generate_poly_commitment(g: Projective<Config>, poly: DensePolynomial<ScalarField>) -> PublicVerificationKey {
+//     let commitments: Vec<Projective<Config>> = poly.coeffs().iter().map(|c: &ScalarField| {
+//         // commitment = g^c
+//         // TOD
+//         // We can compute the pairing of two points on the curve, either monolithically...
+// // let e1 = Bls12_381::pairing(a, b);
+// // // ... or in two steps. First, we compute the Miller loop...
+// // let ml_result = Bls12_381::miller_loop(a, b);
+// // // ... and then the final exponentiation.
+// // let e2 = Bls12_381::final_exponentiation(ml_result).unwrap();
+// // assert_eq!(e1, e2);
+//         g.mul(c)
+//     }).collect::<Vec<_>>();
+//     PublicVerificationKey { coefficients: commitments }
+// }
 
 
 // /// perform DKG with s shares and t threshold
