bce217fc2a7c38099a1a2bdec45a642ce10da2c2
driemworks <tonyrriemer@gmail.com>
2023-04-22 22:49:40 -0500

    refactor function names

--- dkg-wasm/src/lib.rs
+++ dkg-wasm/src/lib.rs
@@ -3,17 +3,20 @@
 //! 
 
 use wasm_bindgen::prelude::*;
-use dkg_core::ser::*;
+use dkg_core::{
+    ser,
+    ser::*,
+};
 
  #[wasm_bindgen]
-pub fn w_keygen(seed: u64, threshold: u8) -> Result<JsValue, serde_wasm_bindgen::Error> {
-    serde_wasm_bindgen::to_value(&s_keygen(seed, threshold))
+pub fn keygen(seed: u64, threshold: u8) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    serde_wasm_bindgen::to_value(&ser::keygen(seed, threshold))
 }
 
 #[wasm_bindgen]
-pub fn w_calculate_secret(be_poly: JsValue) -> Result<JsValue, serde_wasm_bindgen::Error> {
+pub fn calculate_secret(be_poly: JsValue) -> Result<JsValue, serde_wasm_bindgen::Error> {
     let be_poly: BEPoly = serde_wasm_bindgen::from_value(be_poly)?;
-    serde_wasm_bindgen::to_value(&s_calculate_secret(be_poly))
+    serde_wasm_bindgen::to_value(&ser::calculate_secret(be_poly))
 }
 
 #[wasm_bindgen]
@@ -24,7 +27,7 @@ pub fn w_calculate_pubkey(
 ) -> Result<JsValue, serde_wasm_bindgen::Error> {
     // the secret key is encoded as big endian
     let sk_be : Vec<u8> = serde_wasm_bindgen::from_value(secret)?;
-    let pk = s_calculate_pubkey(r1, r2, sk_be);
+    let pk = ser::calculate_pubkey(r1, r2, sk_be);
     serde_wasm_bindgen::to_value(&pk)
 }
 
@@ -35,7 +38,7 @@ pub fn w_calculate_shares(
     coeffs_blob: JsValue,
 ) -> Result<JsValue, serde_wasm_bindgen::Error> {
     let poly: BEPoly = serde_wasm_bindgen::from_value(coeffs_blob)?;
-    serde_wasm_bindgen::to_value(&s_calculate_shares(t, n, poly.coeffs))
+    serde_wasm_bindgen::to_value(&ser::calculate_shares(t, n, poly.coeffs))
 }
 
 #[wasm_bindgen]
@@ -44,11 +47,7 @@ pub fn w_verify_share(
     share_bytes: Vec<u8>, // big endian bytes
     commitment_bytes: Vec<u8>, // serialize_compressed
 ) -> Result<bool, serde_wasm_bindgen::Error> {
-// ) -> bool {
-    // get both as Vec<u8>
-    // let share_be: Vec<u8> = serde_wasm_bindgen::from_value(share_bytes)?;
-    // let commitment: Vec<u8> = serde_wasm_bindgen::from_value(commitment_bytes)?;
-    Ok(s_verify_share(r2, share_bytes, commitment_bytes))
+    Ok(ser::verify_share(r2, share_bytes, commitment_bytes))
 }
 
 #[wasm_bindgen]
@@ -60,14 +59,19 @@ pub fn w_combine_pubkeys(
         serde_wasm_bindgen::from_value(pk1)?;
     let w_pk2: SerializablePublicKey = 
         serde_wasm_bindgen::from_value(pk2)?;
-    let combined_pk = s_combine_pubkeys(w_pk1, w_pk2);
+    let combined_pk = ser::combine_pubkeys(w_pk1, w_pk2);
     serde_wasm_bindgen::to_value(&combined_pk)
 }
 
 #[wasm_bindgen]
 pub fn w_combine_secrets(s1: Vec<u8>, s2: Vec<u8>) -> Vec<u8> {
-    s_combine_secrets(s1, s2)
+    ser::combine_secrets(s1, s2)
 }
+
+// #[wasm_bindgen]
+// pub fn sign() {
+
+// }
 
 #[wasm_bindgen]
 pub fn w_encrypt(
@@ -77,7 +81,7 @@ pub fn w_encrypt(
     pk: JsValue
 ) -> Result<JsValue, serde_wasm_bindgen::Error> {
     let wpk: SerializablePublicKey = serde_wasm_bindgen::from_value(pk)?;
-    serde_wasm_bindgen::to_value(&s_encrypt(seed, r1, msg, wpk))
+    serde_wasm_bindgen::to_value(&ser::encrypt(seed, r1, msg, wpk))
 }
 
 #[wasm_bindgen]
@@ -88,5 +92,5 @@ pub fn w_threshold_decrypt(
 ) -> Result<Vec<u8>, serde_wasm_bindgen::Error> {
     let ciphertext: SerializableCiphertext = 
         serde_wasm_bindgen::from_value(ciphertext_blob)?;
-    Ok(threshold_decrypt(r2, ciphertext, sk))
+    Ok(ser::threshold_decrypt(r2, ciphertext, sk))
 }
